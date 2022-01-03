7891447d09f0598193d21d6a687b86d8e265c6a8
rlcancian <rlcancian@I14-5448>
2022-01-03 14:10:21 -0300

    Renaming app folder

---
+++ applications/teaching/Example_SistemaOperacional02.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Example_SistemasOperacionais02.h
+ * Author: rlcancian
+ *
+ * Created on 12 de abril de 2021, 21:54
+ */
+
+#ifndef EXAMPLE_SISTEMASOPERACIONAIS02_H
+#define EXAMPLE_SISTEMASOPERACIONAIS02_H
+
+#include "../BaseConsoleGenesysApplication.h"
+
+class Example_SistemaOperacional02 :public BaseConsoleGenesysApplication  {
+public:
+	Example_SistemaOperacional02();
+public:
+	virtual int main(int argc, char** argv);
+};
+
+#endif /* EXAMPLE_SISTEMASOPERACIONAIS02_H */
+
